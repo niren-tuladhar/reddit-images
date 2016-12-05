20bae31aa9115d10f959f3108bcd633ed8b315f4
Niren Tuladhar <nirentuladhar@gmail.com>
2016-12-05 22:46:48 +1100

    Added an Image Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Image from './Image';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Image />,
+  document.getElementById('react-container')
 );
